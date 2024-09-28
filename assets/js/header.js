f0473e2360e99ce529d12b56215de34ff8c7663a
Mukesh Sangal <64572-mukesh-kumar3@users.noreply.drupalcode.org>
2024-09-28 20:17:37 +0530

    add changes for the history page

--- assets/js/header.js
+++ assets/js/header.js
@@ -29,16 +29,47 @@ submenuLinks.forEach(function (link) {
     navbarMenu.classList.add('hidden')
   })
 })
-// Slider Js
- var swiper = new Swiper('.default-carousel', {
-   loop: true,
-   pagination: {
-     el: '.swiper-pagination',
-     clickable: true,
-     dots: false
-   },
-   navigation: {
-     nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
-   },
+//Swiper Slider Js
+var swiper = new Swiper('.default-carousel', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dots: false,
+  },
+  // autoplay: {
+  //   delay: 100000, // Delay in milliseconds (e.g., 3000ms = 3 seconds)
+  //   disableOnInteraction: false, // Keeps autoplay active even after user interaction
+  // },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+
+// Pie Hover Effect
+ document.addEventListener('DOMContentLoaded', function () {
+   const segments = document.querySelectorAll('.segment')
+
+   // Add hover effect to each segment
+   segments.forEach((segment) => {
+     segment.addEventListener('mouseover', function () {
+       // Reduce opacity of non-hovered segments
+       segments.forEach((s) => {
+         if (s !== segment) {
+           s.style.fillOpacity = '0.2' // Make other segments transparent
+         }
+       })
+       // Change hovered segment to yellow
+       segment.style.fill = '#0078b6'
+     })
+
+     segment.addEventListener('mouseout', function () {
+       // Reset transparency and colors of all segments
+       segments.forEach((s) => {
+         s.style.fillOpacity = '1' // Reset transparency
+         s.style.fill = s.getAttribute('fill') // Reset original color
+       })
+     })
+   })
  })
